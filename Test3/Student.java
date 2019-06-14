34cfc19bd3ad859ef64107c281da87f3935f0432
Shuu2k <51628169+Shuu2k@users.noreply.github.com>
2019-06-15 00:13:30 +0700

    Add files via upload

---
+++ Test3/Student.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test3;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Dell
+ */
+public class Student implements Serializable {
+    
+    private String name;
+    private int age;
+    private String major;
+    private String email;
+
+    public Student() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
